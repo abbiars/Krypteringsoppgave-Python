90fd8030672bc9dc42049d1e8e6a856ca539cf10
Abbi <abdallaharson@gmail.com>
2026-02-02 10:59:22 +0100

    mangler definisjoner

--- kryp.py
+++ kryp.py
@@ -1,6 +1,32 @@
 
-tekst = input("Skriv det du vil kryptere: ")
+print("Select a mode\n1 = encrypt\n2 = decrypt\n")
+
+prompt = int(input(">"))
+
+def kryptering(Krypter):
 
 
 
-print(f"Resultat:",{tekst[]})
+
+def dekryptering(Dekrypter):
+
+
+
+def checkInt(inp):
+    if inp != type(int):
+        print("the input provided is not a Int")
+    else:
+        return
+
+if prompt == 1:
+    Krypter = input("Skriv det du vil kryptere: ")
+
+elif prompt == 2:
+    Dekrypter = input("Skriv det du vil dekryptere: ")
+else:
+    print("invalid input")
+
+
+
+krypter = input("")
+print(Krypter[::-1])

b98bf34506f4a8e5f438461a60a199da4278a520
Abbi <abdallaharson@gmail.com>
2026-02-06 13:01:56 +0100

    ferdig

--- kryp.py
+++ kryp.py
@@ -3,26 +3,14 @@ print("Select a mode\n1 = encrypt\n2 = decrypt\n")
 
 prompt = int(input(">"))
 
-def kryptering(Krypter):
-
-
-
-
-def dekryptering(Dekrypter):
-
-
-
-def checkInt(inp):
-    if inp != type(int):
-        print("the input provided is not a Int")
-    else:
-        return
 
 if prompt == 1:
     Krypter = input("Skriv det du vil kryptere: ")
+    print(Krypter[::-1])
 
 elif prompt == 2:
     Dekrypter = input("Skriv det du vil dekryptere: ")
+    print(Dekrypter[::-1])
 else:
     print("invalid input")
 
